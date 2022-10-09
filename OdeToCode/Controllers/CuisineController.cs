0a0cf54ea17a1f9d33226ea187584cc4a921c5a7
martinzeroo <martinhobesalu@gmail.com>
2022-10-09 16:04:59 +0300

    Add Razor views

--- OdeToCode/Controllers/CuisineController.cs
+++ OdeToCode/Controllers/CuisineController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OdeToCode.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,15 +9,29 @@ using System.Threading.Tasks;
 
 namespace OdeToCode.Controllers
 {
+    [Log]
     public class CuisineController : Controller
     {
+        //[Authorize]
         public IActionResult Search(string name = "unknown")
         {
             var message = HtmlEncoder.Default.Encode(name);
+            throw new Exception("Something terrible happened:");
 
             //return new EmptyResult();
-            //return Content(message);
-            return File("/css/site.css", "text/css");
+            return Content(message);
+            //return File("/css/site.css", "text/css");
+            //return Json(HtmlEncoder.Default);
+            //return Redirect("https://tthk.ee");
+            //return RedirectToRoute("default",new {controller = "cusine", action = "Second", count = 5});
+            //return RedirectToAction(nameof(Second), new {count = 5})
+            //return View("/home/index");
+            //return RedirectToRoute("default", new { controller = "Home", action = "About" });
+        }
+
+        public IActionResult Second(int count)
+        {
+            return Content($"Teine on siin! {count}");
         }
     }
 }
---
+++ OdeToCode/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToCode.Models;
+
+namespace OdeToCode.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<OdeToCode.Models.RestaurantReview> RestaurantReview { get; set; }
+    }
+}
