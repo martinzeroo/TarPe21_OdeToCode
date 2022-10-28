adf14156de2c57971a33e4669a15d53481de5e06
opilane <opilane@HP-CZC6297B8Q>
2022-10-28 09:02:29 +0300

    Add Review creation capability

--- OdeToCode/Controllers/ReviewsController.cs
+++ OdeToCode/Controllers/ReviewsController.cs
@@ -7,13 +7,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace OdeToCode.Controllers
 {
 	public class ReviewsController : Controller
 	{
 		private readonly ApplicationDbContext _context;
-
 		public ReviewsController(ApplicationDbContext context)
 		{
 			_context = context;
@@ -30,6 +28,23 @@ namespace OdeToCode.Controllers
 			}
 
 			return View(restaurant);
+		}
+
+		[HttpGet]
+		public ActionResult Create(int restaurantId)
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Create(RestaurantReview review)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.RestaurantReviews.Add(review);
+				_context.SaveChanges();
+				return RedirectToAction(nameof(Index), new { id = review.RestaurantId });
+			}
+			return View(review);
 		}
 	}
 }
